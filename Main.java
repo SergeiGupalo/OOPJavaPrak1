76c87afe3700dfad8a5bf7daf794499fa6935c3c
SergeiGupalo <sergeig143@gmail.com>
2022-11-05 22:59:20 +0200

    Rezult

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args){
-
+        int a, b;
+        a = (int) (20 + Math.random()*(30-20));
+        b = (int) (40 + Math.random()*(50-40));
+        System.out.println(a + " " + b);
+        System.out.println(myNumbersProcessing(a, b));
+    }
+    static int myNumbersProcessing(int a, int b){
+        System.out.println(a + b);
+        return a * b;
     }
 }
